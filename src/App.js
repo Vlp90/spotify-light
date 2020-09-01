ddd7c1e484a4c0f58f65d9455da1dff4a75b8358
Vlp90 <vladimir.leloup@gmail.com>
2020-09-01 12:54:41 +0200

    all good except player

--- src/App.js
+++ src/App.js
@@ -1,74 +1,69 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import SpotifyWebApi from "spotify-web-api-js";
+import { useStateValue } from "./StateProvider";
+import Player from "./Player";
+import { getTokenFromResponse } from "./spotify";
 import "./App.css";
 import Login from "./Login";
-import { getTokenFromUrl } from "./spotify";
-import SpotifyWebApi from "spotify-web-api-js";
 
-import Player from "./Player";
-
-import { useStateValue } from "./StateProvider";
-
-// create an object to interact with spotify api
-// npm i spotify-web-api-js
-const spotify = new SpotifyWebApi();
+const s = new SpotifyWebApi();
 
 function App() {
-  // run Code based on a given condition
-  // const [token, setToken] = useState(null);
-  const [{ user, token }, dispatch] = useStateValue();
+  const [{ token }, dispatch] = useStateValue();
 
   useEffect(() => {
-    const hash = getTokenFromUrl();
+    // Set token
+    const hash = getTokenFromResponse();
     window.location.hash = "";
-    const _token = hash.access_token;
+    let _token = hash.access_token;
 
     if (_token) {
-      // setToken(_token);
+      s.setAccessToken(_token);
 
       dispatch({
         type: "SET_TOKEN",
         token: _token,
       });
 
-      spotify.setAccessToken(_token);
+      s.getPlaylist("37i9dQZEVXcD2Y9wKhL8D6").then((response) =>
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response,
+        })
+      );
 
-      // connect spotify to react
-      spotify.getMe().then((user) => {
-        // console.log("USER Connected", user);
+      s.getMyTopArtists().then((response) =>
+        dispatch({
+          type: "SET_TOP_ARTISTS",
+          top_artists: response,
+        })
+      );
 
+      dispatch({
+        type: "SET_SPOTIFY",
+        spotify: s,
+      });
+
+      s.getMe().then((user) => {
         dispatch({
           type: "SET_USER",
-          user: user,
+          user,
         });
       });
 
-      spotify.getUserPlaylists().then((playlists) => {
-        // console.log("USER Connected", user);
-
+      s.getUserPlaylists().then((playlists) => {
         dispatch({
           type: "SET_PLAYLISTS",
-          playlists: playlists,
+          playlists,
         });
-
-        spotify.getPlaylist("37i9dQZEVXcD2Y9wKhL8D6").then((response) => {
-          dispatch({
-            type: "SET_DISCOVER_WEEKLY",
-            discover_weekly: response,
-          });
-        });
-
-        //
       });
     }
-    // console.log("I have a token ------>>>", token);
-  }, []);
-
-  // console.log("USER Connected", user);
-  console.log("USER TOKEN", token);
+  }, [token, dispatch]);
 
   return (
     <div className="app">
-      {token ? <Player spotify={spotify} /> : <Login />}
+      {!token && <Login />}
+      {token && <Player spotify={s} />}
     </div>
   );
 }
--- src/spotify.js
+++ src/spotify.js
@@ -16,16 +16,18 @@ const scopes = [
   "user-modify-playback-state",
 ];
 
-// pull the access Token out from the URL
-export const getTokenFromUrl = () => {
-  return window.location.hash.substring(1).split("&").reduce((initial, item)=> {
-      let parts = item.split('=');
-      initial[parts[0]] = decodeURIComponent(parts[1])
+export const getTokenFromResponse = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+
       return initial;
-  }, {});
+    }, {});
 };
 
-// Go to sportify login page then log in and redirect to my website
-export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
   "%20"
 )}&response_type=token&show_dialog=true`;
