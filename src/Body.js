9335e1401d780281c851708aba19064aaafdbe1a
Vlp90 <vladimir.leloup@gmail.com>
2020-09-01 11:22:41 +0200

    header search and avatar username style ok

--- src/Body.js
+++ src/Body.js
@@ -1,11 +1,13 @@
 // rfce
 import React from "react";
 import "./Body.css";
+import Header from './Header.js'
 
-function Body() {
+function Body({spotify}) {
   return (
     <div className="body">
-      <h1>Body</h1>
+    <Header spotify={spotify} />
+      <h1>i AM Body</h1>
     </div>
   );
 }
